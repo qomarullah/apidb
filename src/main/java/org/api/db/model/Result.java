9968af38a8984efd04aa670675d2eb5aea0328fd
MFS Technology <qomarullah.mail@gmail.com>
2018-10-15 05:54:14 +0700

    remove some code

--- src/main/java/org/api/db/model/Result.java
+++ src/main/java/org/api/db/model/Result.java
@@ -9,8 +9,8 @@ public class Result implements Serializable{
     public String status = "";
     public String desc="";
     public int count = -1;
-    public int count_total = -1;
-    public int pages = -1;
+    //public int count_total = -1;
+    //public int pages = -1;
     public List<Object> results = new ArrayList<>();
 
 }
